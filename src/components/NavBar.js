4ef45528316d3f686b53e8f5500737ce2d1ebfa0
lopeselio <lopeselio@gmail.com>
2020-07-19 12:17:33 +0530

    added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,43 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-const NavBar = () => {
-    return(
-        <div>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                    Account Aggregator is so cool
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        </div>
-    )
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h7" className={classes.title}>
+            XYZ Bank
+          </Typography>
+          <Button color="inherit">Customers</Button>
+          <Button color="inherit">Products</Button>
+          <Button color="inherit">Consent Types</Button>
+
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-export default NavBar;
